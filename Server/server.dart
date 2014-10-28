17856faa28d51a218722307a08d63fe838b9bb09
mkocs <mko.mko31@gmail.com>
2014-10-28 23:18:06 +0100

    Added basic http server

---
+++ Server/server.dart
@@ -0,0 +1,5 @@
+import 'src/con/http/http_server.dart';
+
+main(){
+  Http_Server http = new Http_Server(1337);
+}
---
+++ Server/src/con/database/db.dart
@@ -0,0 +1,9 @@
+library db;
+
+import 'dart:html';
+
+class DataBase {
+  void open_connection(){
+
+  }
+}
