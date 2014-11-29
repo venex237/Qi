bf42f706015984ffb9e904a3d898600da4f51de2
Michael Köppl <mko.mko31@gmail.com>
2014-11-29 19:26:50 +0100

    Added HTTPS server capability

--- public/javascripts/ajax.js
+++ public/javascripts/ajax.js
@@ -2,26 +2,25 @@
  * fills the div via an ajax request
  */
 function ajax(dir, file){
+  console.log(dir);
+  console.log(file);
 
-    console.log(dir);
-    console.log(file);
-
-    $.ajax({
-      type:'GET',
-      url:('/ajax/' + dir + file + '.txt'),
-      success: function(data){
-        $('#panelcontainer').html(data);
-      }
-    });
+  $.ajax({
+    type:'GET',
+    url:('/ajax/' + dir + file + '.txt'),
+    success: function(data){
+      $('#panelcontainer').html(data);
+    }
+  });
 
 
-    /*
-     * changing URL according to the used panel
-     *
-     * documentation:
-     * https://developer.mozilla.org/en-US/docs/Web/Guide/API/DOM/Manipulating_the_browser_history#The_pushState()_method
-     */
-    history.pushState(history.state, document.title, '?p=' + file);
+  /*
+   * changing URL according to the used panel
+   *
+   * documentation:
+   * https://developer.mozilla.org/en-US/docs/Web/Guide/API/DOM/Manipulating_the_browser_history#The_pushState()_method
+   */
+  history.pushState(history.state, document.title, '?p=' + file);
 }
 
 /*
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Qi' });
+  console.normal('GET request for /  ::--> returned index.jade');
 });
 
 module.exports = router;
