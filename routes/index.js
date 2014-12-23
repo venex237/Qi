4a6c6e181d28a42725d8f68a0a19371dce69a1e1
Michael Köppl <mko.mko31@gmail.com>
2014-12-23 01:01:46 +0100

    Transformed index.html to jade

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
   //res.render('index', { title: 'Qi' });
-  console.normal('GET request for /  ::--> returned index.html');
-  res.render('index.html');
+  console.log('GET request for /  ::--> returned index.jade');
+  res.render('index.jade', {"title": "Qi"});
 });
 
 module.exports = router;
