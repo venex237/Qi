6cc9cd1698f5ea1b7d47f4d8266874ac34bdc6be
Michael Köppl <mko.mko31@gmail.com>
2014-12-24 12:35:27 +0100

    Removed scribe logging

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var http = require('http');
 var https = require('https');
 var path = require('path');
 var favicon = require('serve-favicon');
-var scribe = require('scribe'); /*npm install git+https://github.com/bluejamesbond/Scribe.js.git*/
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var fs = require('fs');
@@ -40,66 +39,15 @@ var options = {key: privateKey, cert: certificate};
 // initialise express
 var app = express();
 
-/* ==================================================================================
- *                              SCRIBE LOGGING
- *                  https://github.com/bluejamesbond/Scribe.js
- */
-
-// Configuration --> logging
-// --------------
-scribe.configure(function(){
-    scribe.set('app', 'Qi');
-    scribe.set('logPath', 'logs/');                      // Doublecheck
-    scribe.set('defaultTag', 'QI');
-    scribe.set('divider', ':::');
-    scribe.set('identation', 5);                          // Identation before console messages
-
-    scribe.set('maxTagLength', 30);                       // Any tags that have a length greather than
-                                                          // 30 characters will be ignored
-
-    scribe.set('mainUser', 'root');                       // Username of the account which is running
-                                                          // the NodeJS server
-});
-
-// Create Loggers
-// --------------
-scribe.addLogger('log', true , true, 'green');            // (name, save to file, print to console,
-scribe.addLogger('realtime', true, true, 'underline');    // tag color)
-scribe.addLogger('high', true, true, 'magenta');
-scribe.addLogger('normal', true, true, 'white');
-scribe.addLogger('low', true, true, 'grey');
-scribe.addLogger('info', true, true, 'cyan');
-
-/*                              END SCRIBE LOGGING
- * ==================================================================================
- */
-
-
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'jade');
+app.set('development', function () { app.locals.pretty = true; });
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-
-/*
- * SCRIBE
- */
-app.use(scribe.express.logger(function(req, res){         // Express.js access log
-    return true;                                          // Filter out any Express messages
-}));
-
-// Control Panel
-// WARNING: ExpressJS must be installed for this to work
-// You also need to start an ExpressJS server in order for
-// this to work.
-// --------------
-app.get('/log', scribe.express.controlPanel());           // Enable web control panel
-/*
- * SCRIBE
- */
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Qi' });
-  console.normal('GET request for /  ::--> returned index.jade');
+  res.render('index');
+  console.log('GET request for /  ::--> returned index.jade');
 });
 
 module.exports = router;
