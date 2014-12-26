aacad7affe1bbefe46a6c46f96bcf49b65e015d8
Michael Köppl <mko.mko31@gmail.com>
2014-12-26 14:42:28 +0100

    Add misc.js (provisional)
    misc.js for now only checks if a string is an email address

---
+++ public/javascripts/misc.js
@@ -0,0 +1,5 @@
+// return true if email is valid
+function checkIfEmailInString(text) {
+    var re = /(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/;
+    return re.test(text);
+}
