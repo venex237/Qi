a4d538d20f58fe6d8ae42deefe10db2849026d09
Michael Köppl <mko.mko31@gmail.com>
2014-12-26 14:46:04 +0100

    Update welcome.js
    - divs are now exchanged correctly
    - add login button click
    - add keypress listeners

--- public/javascripts/welcome/welcome.js
+++ public/javascripts/welcome/welcome.js
@@ -1,15 +1,48 @@
+
+// all click listeners for the welcome page
+// for the "links" to change the forms between login and register
+// for the registration and login button
 function set_welcome_click_listeners(){
 	$("#gotoreg_login").bind("click", function () {
-		$(".loginpanel").replaceWith($(".registerpanel"));
+		$(":input").val("");
+		$("#regbutton_reg").val("Create my account");
+		$(".registerpanel").addClass("active");
+		$(".loginpanel").removeClass("active");
+		$(".loginpanel").hide();
 		$(".registerpanel").show();
 	});
 
 	$("#gotologin_reg").bind("click", function () {
-		$(".registerpanel").replaceWith($(".loginpanel"));
+		$(":input").val("");
+		$("#loginbutton_login").val("Log in");
+		$(".loginpanel").addClass("active");
+		$(".registerpanel").removeClass("active");
+		$(".registerpanel").hide();
 		$(".loginpanel").show();
 	});
 
 	$("#regbutton_reg").click(function(){
 		register();
+	});
+
+	$("#loginbutton_login").click(function(){
+		login();
+	});
+}
+
+// listen for enter on the inputs
+function set_welcome_keypress_listeners(){
+	$(".login").bind("keypress", function (e) {
+	    var key = e.which || e.keyCode;
+	    if (key == 13) { // 13 is enter
+	      login();
+	    }
+	});
+
+	$(".reg").bind("keypress", function (e) {
+	    var key = e.which || e.keyCode;
+	    if (key == 13) { // 13 is enter
+	      register();
+	    }
 	});
 }
